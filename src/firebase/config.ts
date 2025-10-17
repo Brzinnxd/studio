ef983d1f3b00c01651d0b2627fe2b20387478a6e
Arthur Vieira <arthur.vieirask@gmail.com>
2025-10-17 18:00:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9431941049-8bc8c",
+  "appId": "1:1084168560879:web:085832e20c7cb145c67b1a",
+  "apiKey": "AIzaSyAPU3Lw4zFOqpd4ySeSY7NrI76IzhI243Y",
+  "authDomain": "studio-9431941049-8bc8c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1084168560879"
+};
